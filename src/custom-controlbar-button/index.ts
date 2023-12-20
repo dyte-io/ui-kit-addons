ceffd40afcce6f12d21d96335b22566cfb70b25e
Tushar Sonawane <tushar@dyte.io>
2023-12-20 15:53:48 +0530

    fix: add more-menu position for CustomControlbarButton

--- src/custom-controlbar-button/index.ts
+++ src/custom-controlbar-button/index.ts
@@ -1,78 +1,89 @@
-import { DyteUIBuilder, UIConfig } from "@dytesdk/ui-kit";
-import { Meeting } from "@dytesdk/ui-kit/dist/types/types/dyte-client";
+import { DyteUIBuilder, UIConfig } from '@dytesdk/ui-kit';
+import { Meeting } from '@dytesdk/ui-kit/dist/types/types/dyte-client';
 
 export interface CustomControlbarButtonArgs {
-    position: "left" | "right" | "center";
-    icon: string;
-    label: string;
-    onClick: () => void;
-    attributes?: { [key: string]: any };
+	position: 'left' | 'right' | 'center' | 'more-menu';
+	icon: string;
+	label: string;
+	onClick: () => void;
+	attributes?: { [key: string]: any };
 }
 
 export default class CustomControlbarButton {
-    meeting?: Meeting;
+	meeting?: Meeting;
 
-    position: "left" | "right" | "center";
+	position: 'left' | 'right' | 'center' | 'more-menu';
 
-    icon: string;
+	icon: string;
 
-    label: string;
+	label: string;
 
-    onClick: () => void;
+	onClick: () => void;
 
-    attributes: { [key: string]: any };
+	attributes: { [key: string]: any };
 
-    constructor(args: CustomControlbarButtonArgs) {
-        this.icon = args.icon;
-        this.label = args.label;
-        this.onClick = args.onClick;
-        this.position = args.position;
-        this.attributes = args.attributes || {};
-    }
+	constructor(args: CustomControlbarButtonArgs) {
+		this.icon = args.icon;
+		this.label = args.label;
+		this.onClick = args.onClick;
+		this.position = args.position;
+		this.attributes = args.attributes || {};
+	}
 
-    async unregister() {
-        // TODO: Remove the changer from the body
-    }
+	async unregister() {
+		// TODO: Remove the changer from the body
+	}
 
-    addControlBarButton(selector: any, attributes: { [key: string]: any }) {
-        selector.add("dyte-controlbar-button", {
-            id: this.label,
-            label: this.label,
-            icon: this.icon,
-            // @ts-ignore
-            onClick: this.onClick,
-            ...attributes
-        });
-    }
+	addControlBarButton(selector: any, attributes: { [key: string]: any }) {
+		selector.add('dyte-controlbar-button', {
+			id: this.label,
+			label: this.label,
+			icon: this.icon,
+			// @ts-ignore
+			onClick: this.onClick,
+			...attributes,
+		});
+	}
 
-    register(config: UIConfig, meeting: Meeting, getBuilder: (c: UIConfig) => DyteUIBuilder) {
-        this.meeting = meeting;
-        // Add buttons with config
-        const builder = getBuilder(config);
-        const controlBar = builder.find(`div#controlbar-${this.position}`);
-        if (!controlBar) return config;
-        this.addControlBarButton(controlBar, this.attributes);
+	register(config: UIConfig, meeting: Meeting, getBuilder: (c: UIConfig) => DyteUIBuilder) {
+		this.meeting = meeting;
+		// Add buttons with config
+		const builder = getBuilder(config);
 
-        // Add button in more menu for different screen sizes
-        const mdMoreItems = builder.find("dyte-more-toggle", {
-            activeMoreMenu: true,
-            md: true
-        });
-        this.addControlBarButton(mdMoreItems, {
-            variant: "horizontal",
-            slot: "more-elements"
-        });
+		if (this.position === 'more-menu') {
+			const lgMoreItems = builder.find('dyte-more-toggle', {
+				activeMoreMenu: true,
+			});
+			this.addControlBarButton(lgMoreItems, {
+				variant: 'horizontal',
+				slot: 'more-elements',
+			});
+		} else {
+			const controlBar = builder.find(`div#controlbar-${this.position}`);
+			if (!controlBar) return config;
+			this.addControlBarButton(controlBar, this.attributes);
+		}
 
-        const smMoreItems = builder.find("dyte-more-toggle", {
-            activeMoreMenu: true,
-            sm: true
-        });
-        this.addControlBarButton(smMoreItems, {
-            variant: "horizontal",
-            slot: "more-elements"
-        });
+		// Add button in more menu for different screen sizes
+		const mdMoreItems = builder.find('dyte-more-toggle', {
+			activeMoreMenu: true,
+			md: true,
+		});
+		this.addControlBarButton(mdMoreItems, {
+			variant: 'horizontal',
+			slot: 'more-elements',
+		});
 
-        // Return the updated config
-        return builder.build();
-    }
+		const smMoreItems = builder.find('dyte-more-toggle', {
+			activeMoreMenu: true,
+			sm: true,
+		});
+		this.addControlBarButton(smMoreItems, {
+			variant: 'horizontal',
+			slot: 'more-elements',
+		});
+
+		// Return the updated config
+		return builder.build();
+	}
 }
